f5f71123a5794685db0afcf447c942915e3acded
Sarathamyres <thamyressara071@gmail.com>
2024-08-01 12:40:39 -0300

    Quase finalizado. Falta apenas debugar

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ document.querySelectorAll("button[data-popup]").forEach((button) => {
   button.addEventListener("click", function () {
     const popupId = this.getAttribute("data-popup");
     document.getElementById(popupId).style.display = "flex";
+    document.body.classList.add('no-scroll')
+
     document.getElementById('hide').setAttribute('data-popup-id', popupId);
   });
 });
@@ -10,5 +12,6 @@ document.querySelectorAll('.icone-hide').forEach((hide) => {
   hide.addEventListener('click', function () {
     const popupId = this.getAttribute('data-popup-id');
     document.getElementById(popupId).style.display = "none";
+    document.body.classList.remove('no-scroll')
   });
 });
